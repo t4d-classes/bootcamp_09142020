ccc3bf5e1416da0acc7bd149befe1bc3ded1b41c
Eric Greene <eric@t4d.io>
2020-09-19 08:39:14 -0400

    add lab for calc history and clear button

--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -5,15 +5,29 @@ import { useSelector, useDispatch, Provider } from 'react-redux';
 
 const ADD_ACTION = 'ADD';
 const SUBTRACT_ACTION = 'SUBTRACT';
+const MULTIPLY_ACTION = 'MULTIPLY';
+const DIVIDE_ACTION = 'DIVIDE';
+const CLEAR_ACTION = 'CLEAR';
 
-export interface CalcOpAction extends Action {
+export interface CalcOpAction extends Action<string> {
   payload: {
     num: number,
   },
 }
 
-export type CalcOpActionCreator = (num: number) => CalcOpAction;
+export type CalcAction = Action<string>;
 
+export type CalcOpActionCreator = (num: number) => CalcOpAction;
+export type CalcActionCreator = () => CalcAction;
+
+export function isCalcOpAction(action: Action<string>): action is CalcOpAction {
+  return [ ADD_ACTION, SUBTRACT_ACTION, MULTIPLY_ACTION, DIVIDE_ACTION ].includes(action.type);
+}
+
+export function isCalcAction(action: Action<string>): action is CalcOpAction {
+  return action.type === CLEAR_ACTION;
+}
+ 
 export const createAddAction: CalcOpActionCreator = (num) => ({
   type: ADD_ACTION,
   payload: {
@@ -28,33 +42,83 @@ export const createSubtractAction: CalcOpActionCreator = (num) => ({
   }
 });
 
-export function isCalcOpAction(action: Action<string>): action is CalcOpAction {
-  return action.type === ADD_ACTION || action.type === SUBTRACT_ACTION;
-}
+export const createMultiplyAction: CalcOpActionCreator = (num) => ({
+  type: MULTIPLY_ACTION,
+  payload: {
+    num,
+  }
+});
+
+export const createDivideAction: CalcOpActionCreator = (num) => ({
+  type: DIVIDE_ACTION,
+  payload: {
+    num,
+  }
+});
+
+export const createClearAction: CalcActionCreator = () => ({
+  type: CLEAR_ACTION,
+});
+
+export type CalcHistoryEntry = {
+  name: string,
+  value: number,
+};
 
 // state data
 export type CalcToolState = {
   result: number,
+  history: CalcHistoryEntry[],
 };
+
+const initialState = { result: 0, history: [], };
 
 // stateful logic
 // newState <- reducer(currentState, action)
-export const calcToolReducer: Reducer<CalcToolState, CalcOpAction> = (state = { result: 0 }, action) => {
+export const calcToolReducer: Reducer<CalcToolState, CalcOpAction | CalcAction> = (state = initialState, action) => {
 
-  switch (action.type) {
-    case ADD_ACTION:
-      return {
-        ...state,
-        result: state.result + action.payload.num,
-      };
-    case SUBTRACT_ACTION:
-      return {
-        ...state,
-        result: state.result - action.payload.num,
-      };
-    default:
-      return state;
+  let newState = state;
+
+  if (isCalcAction(action)) {
+    newState = initialState;
   }
+
+  if (isCalcOpAction(action)) {
+
+    switch (action.type) {
+      case ADD_ACTION:
+        newState = {
+          ...state,
+          result: state.result + action.payload.num,
+          history: [ ...state.history, { name: '+', value: action.payload.num } ],
+        };
+        break;
+      case SUBTRACT_ACTION:
+        newState = {
+          ...state,
+          result: state.result - action.payload.num,
+          history: [ ...state.history, { name: '-', value: action.payload.num } ],
+        };
+        break;
+      case MULTIPLY_ACTION:
+        newState = {
+          ...state,
+          result: state.result - action.payload.num,
+          history: [ ...state.history, { name: '*', value: action.payload.num } ],
+        };
+        break;
+      case DIVIDE_ACTION:
+        newState = {
+          ...state,
+          result: state.result - action.payload.num,
+          history: [ ...state.history, { name: '/', value: action.payload.num } ],
+        };
+        break;
+    }
+
+  }
+
+  return newState;
 
 };
 
@@ -62,43 +126,57 @@ export const calcToolStore = createStore(calcToolReducer);
 
 export type CalcToolProps = {
   result: number,
+  history: CalcHistoryEntry[],
   onAdd: (num: number) => void,
   onSubtract: (num: number) => void,
+  onMultiply: (num: number) => void,
+  onDivide: (num: number) => void,
+  onClear: () => void,
 };
 
-// presentational component
 export function CalcTool(props: CalcToolProps) {
 
   const [ numInput, setNumInput ] = useState(0);
 
   return (
-    <form>
-      <div>Result: <span>{props.result}</span></div>
-      <div><label>Num Input:
-          <input type="number" value={numInput}
-            onChange={e => setNumInput(e.target.valueAsNumber)} />
-      </label></div>
-      <fieldset>
-        <button type="button" onClick={() => props.onAdd(numInput)}>+</button>
-        <button type="button" onClick={() => props.onSubtract(numInput)}>-</button>
-      </fieldset>
-    </form>
+    <>
+      <form>
+        <div>Result: <span>{props.result}</span></div>
+        <div><label>Num Input:
+            <input type="number" value={numInput}
+              onChange={e => setNumInput(e.target.valueAsNumber)} />
+        </label></div>
+        <fieldset>
+          <button type="button" onClick={() => props.onAdd(numInput)}>+</button>
+          <button type="button" onClick={() => props.onSubtract(numInput)}>-</button>
+          <button type="button" onClick={() => props.onMultiply(numInput)}>*</button>
+          <button type="button" onClick={() => props.onDivide(numInput)}>/</button>
+          <button type="button" onClick={() => props.onClear()}>Clear</button>
+        </fieldset>
+      </form>
+      <ul>
+        {props.history.map( (historyEntry, index) =>
+          <li key={index}>{historyEntry.name} {historyEntry.value}</li>)}
+      </ul>
+    </>
   );
 }
 
 export function CalcToolContainer() {
 
   const result = useSelector<CalcToolState, number>(state => state.result);
+  const history = useSelector<CalcToolState, CalcHistoryEntry[]>(state => state.history);
 
-  // boundActionsMap.onAdd = (num) => dispatch(createAddAction(num))
-  // boundActionsMap.onSubtract = (num) => dispatch(createSubtractAction(num))
   const boundActionsMap = bindActionCreators({
     onAdd: createAddAction,
     onSubtract: createSubtractAction,
+    onMultiply: createMultiplyAction,
+    onDivide: createDivideAction,
+    onClear: createClearAction,
   }, useDispatch());
 
   return (
-    <CalcTool result={result} {...boundActionsMap} />
+    <CalcTool result={result} history={history} {...boundActionsMap} />
   );
 
 }
