c5ba87950ea8fb00f0ce6ddd70838b947c3b78c5
Eric Greene <eric@t4d.io>
2020-09-22 17:41:52 -0400

    add thunk

--- demo-app/src/stores/carToolStore.ts
+++ demo-app/src/stores/carToolStore.ts
@@ -1,4 +1,6 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import { carToolReducer } from '../reducers/carToolReducers';
 
